138f5a759feebe887a5432ca054146c745f06fa9
Dibwab <Dibwab.github.com>
2020-05-13 21:26:46 +0530

    error removed

--- dictionary.py
+++ dictionary.py
@@ -5,17 +5,17 @@ data= json.load(open("data.json"))
 def translate(w):
     w=w.lower()
     if(w in data):
-        return(translate(w))
+        return(data[w])
     elif len(get_close_matches(w,data.keys()))>0:
-        x=import("Did you mean %s instead? Y or N" % get_close_matches(w,data.keys())[0])
-        if x=='Y'||x=='y':
-            return translate(get_close_matches(w,data.keys())[0]))
+        x=input("Do you mean %s instead? Y or N :  " % get_close_matches(w,data.keys())[0])
+        if (x =='Y'or x =='y'):
+            return translate(get_close_matches(w,data.keys())[0])
         else:
             print("Try again")
     else:
         return("No such word found")
-y='N'
-while():
-    word=import("Enter word: ")
+y='Y'
+while(y=='Y'or y=='y'):
+    word=input("Enter word: ")
     print(translate(word))
-    y=import("Do you wanna search for another word? Y or N")
+    y=input("Do you wanna search for another word? Y or N :  ")
